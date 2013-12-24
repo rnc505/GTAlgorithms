26108b51ff0b2771f672adb90310af2a0b5b178b
Robby Cohen <robert.n.cohen@vanderbilt.edu>
2013-12-24 12:07:02 -0500

    Initial Commit

---
+++ GTAlgorithms/GTAlgorithms.h
@@ -0,0 +1,13 @@
+//
+//  GTAlgorithms.h
+//  GTAlgorithms
+//
+//  Created by Robby Cohen on 12/24/13.
+//  Copyright (c) 2013 Robby Cohen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTAlgorithms : NSObject
+
+@end
---
+++ GTAlgorithms/GTAlgorithms.m
@@ -0,0 +1,13 @@
+//
+//  GTAlgorithms.m
+//  GTAlgorithms
+//
+//  Created by Robby Cohen on 12/24/13.
+//  Copyright (c) 2013 Robby Cohen. All rights reserved.
+//
+
+#import "GTAlgorithms.h"
+
+@implementation GTAlgorithms
+
+@end
